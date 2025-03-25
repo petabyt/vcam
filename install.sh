44ce3a75b85d1154233088a5ef1412023954a786
Daniel C <brikbusters@gmail.com>
2025-03-24 21:13:33 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+# Script to compile and install
 set -e
 
 cd ~
 rm -rf /tmp/vcam
-git clone https://github.com/petabyt/vcam /tmp/vcam
+git clone https://github.com/petabyt/vcam --depth 1 --recurse-submodules /tmp/vcam
 cd /tmp/vcam
 make install
